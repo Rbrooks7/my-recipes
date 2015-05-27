b9e7b42ca51308cd3870d8677a99ac30b3ab9bf4
rebrooks7 <ross@yourgreenpal.com>
2015-05-27 23:11:32 +0000

    add nav functionality for ingredients

--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -1,5 +1,10 @@
 class StylesController < ApplicationController
   
+  def show
+    @style = Style.find(params[:id])
+    @recipes = @style.recipes.paginate(page: params[:page], per_page: 4)
+  end
+  
   def new 
     @style = Style.new
   end
@@ -13,6 +18,8 @@ class StylesController < ApplicationController
       render 'new'
     end
   end
+  
+  
   
   private
     def style_params
